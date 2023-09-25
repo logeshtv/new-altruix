dac9c6421bd012715f6cd982af0e0e41af8a6447
logeshloki585 <Logeshtv21@gmail.com>
2023-09-25 21:28:58 +0530

    k

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  console.log(process.env.MONGODB_URI)
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
